ba93ed86eb9169ae7a723a2d743ac1efa4161192
Tuan Pham <tuanbk08vn@gmail.com>
2017-07-11 10:07:25 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 	$(document).ready(function(){
 		$('#btn').click(function(){
-			
+			fffffffffffffffffffffffffffffffffff;
 			alert("123");
 			var xxx = document.getElementByName("a[]");
 			alert(xxx);
