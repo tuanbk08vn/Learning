e8db34172a54be87c973f0d9260e6b0eebd803e3
Tuan Pham <tuanbk08vn@gmail.com>
2017-07-11 10:20:21 +0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-
-	$(document).ready(function(){
-		$('#btn').click(function(){
-			fffffffffffffffffffffffffffffffffff;
-			alert("123");
-			var xxx = document.getElementByName("a[]");
-			alert(xxx);
-		});
-	})
