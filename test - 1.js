8924da4b4a570ea76f7698980cd7cf200128e1bf
LANWORKS\tuan.pham <tuanbk08vn@gmail.com>
2017-07-11 10:15:15 +0700

    add new file 1

---
+++ test - 1.js
@@ -0,0 +1,9 @@
+
+	$(document).ready(function(){
+		$('#btn').click(function(){
+			fffffffffffffffffffffffffffffffffff;
+			alert("123");
+			var xxx = document.getElementByName("a[]");
+			alert(xxx);
+		});
+	})
