ff9be8e8afd6d5b298364d0460188885e13f7a61
LANWORKS\tuan.pham <tuanbk08vn@gmail.com>
2017-07-11 10:18:51 +0700

    update file

--- test - 1.js
+++ test - 1.js
@@ -1,9 +1,1 @@
-
-	$(document).ready(function(){
-		$('#btn').click(function(){
-			fffffffffffffffffffffffffffffffffff;
-			alert("123");
-			var xxx = document.getElementByName("a[]");
-			alert(xxx);
-		});
-	})
+blalaallalalalalala
